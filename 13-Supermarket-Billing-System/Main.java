0cea36370ecd6eff1c74987b5d0c8748707180e5
Yuvraj.R <yuvrajmysuru@gmail.com>
2026-03-14 17:45:04 +0530

    Update Main.java

--- 13-Supermarket-Billing-System/Main.java
+++ 13-Supermarket-Billing-System/Main.java
@@ -48,3 +48,53 @@ public class Main {
         sc.close();
     }
 }
+
+
+# Supermarket Billing System 🛒
+
+This is a simple Java console-based Supermarket Billing System.
+
+The program allows users to select items from a menu and automatically calculates the total bill. The system continues running until the user chooses the checkout option.
+
+## Available Items
+
+1. Rice – ₹50  
+2. Milk – ₹30  
+3. Bread – ₹40  
+
+## Features
+
+- Menu-driven billing system
+- Add items to the cart
+- Automatically updates total bill
+- Checkout option to display final bill
+- Simple and beginner-friendly logic
+
+## Concepts Used
+
+- Java Basics
+- `Scanner` class for user input
+- `if-else` conditional statements
+- `do-while` loop
+- Basic arithmetic operations
+
+## Sample Output
+SUPERMARKET BILLING SYSTEM 
+
+- Rice - ₹50
+
+- Milk - ₹30
+
+- Bread - ₹40
+
+   Checkout
+
+Choose item: 1
+Rice added to cart
+
+Choose item: 2
+Milk added to cart
+
+Choose item: 4
+Total Bill: ₹80
+Thank you for shopping Yuvi 😎
